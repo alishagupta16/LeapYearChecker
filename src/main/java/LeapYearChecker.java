6c5cb55bc70e4cd759534a25387c3456ec7a44dc
gupta.alisha16 <gupta.alisha16@gmail.com>
2020-05-17 00:42:07 +0530

    final criteria for divisibility by 400 added

--- src/main/java/LeapYearChecker.java
+++ src/main/java/LeapYearChecker.java
@@ -2,7 +2,7 @@
 public class LeapYearChecker {
 
 	public boolean isLeapYear(int year) {
-		boolean result = year % 4 == 0 && year % 100 != 0;
+		boolean result = year % 4 == 0 && (year % 100 != 0 || year % 400 == 0);
 		return result;
 	}
 
--- src/test/java/LeapYearCheckerTest.java
+++ src/test/java/LeapYearCheckerTest.java
@@ -23,5 +23,13 @@ public class LeapYearCheckerTest extends TestCase{
 	public void testLeapYear_NotLeapYear2() throws Exception {
 		assertFalse(testObject.isLeapYear(1700));
 	}
+	
+	public void testLeapYear_NotLeapYear3() throws Exception {
+		assertFalse(testObject.isLeapYear(2100));
+	}
+	
+	public void testLearYear_YesLeapYear2() throws Exception {
+		assertTrue(testObject.isLeapYear(2000));
+	}
 
 }
